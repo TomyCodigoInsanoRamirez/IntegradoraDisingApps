0ffb70a7a2520325cd16a088007131d0b0aea0eb
Cyan Giselle <20233tn176@utez.edu.mx>
2024-11-24 00:22:41 -0600

    cambios recientes

--- Pantallas_sipal/app/src/main/java/com/example/pantallas_sipal/MainActivity.kt
+++ Pantallas_sipal/app/src/main/java/com/example/pantallas_sipal/MainActivity.kt
@@ -18,20 +18,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnIniciarSesion.setOnClickListener{
-            val intent = Intent(this@MainActivity, Vista_Alumno::class.java)
+            val intent = Intent(this@MainActivity, Vista_alumno::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(intent)
         }
     }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.barra_menu_vista_alumno,menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.algo -> finish()
-        }
-        return super.onOptionsItemSelected(item)
-    }
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.barra_menu_vista_alumno,menu)
+//        return super.onCreateOptionsMenu(menu)
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when(item.itemId){
+//            R.id.algo -> finish()
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
 }
--- Pantallas_sipal/app/src/main/java/com/example/pantallas_sipal/Vista_Alumno.kt
+++ Pantallas_sipal/app/src/main/java/com/example/pantallas_sipal/Vista_Alumno.kt
@@ -1,15 +1,20 @@
 package com.example.pantallas_sipal
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.pantallas_sipal.databinding.ActivityVistaAlumnoBinding
 
-class Vista_Alumno : AppCompatActivity() {
+class Vista_alumno : AppCompatActivity() {
     private lateinit var binding: ActivityVistaAlumnoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityVistaAlumnoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
 
     }
 }
